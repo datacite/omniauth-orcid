3add8fa701738a3b1389429a81a59b3bbf7d01f1
Martin Fenner <mfenner@plos.org>
2014-12-21 00:45:20 +0100

    change default scope and token_url to values that work for non-members. Add info.name

--- lib/omniauth/strategies/orcid.rb
+++ lib/omniauth/strategies/orcid.rb
@@ -6,27 +6,27 @@ module OmniAuth
   module Strategies
     class ORCID < OmniAuth::Strategies::OAuth2
 
-      DEFAULT_SCOPE = '/orcid-bio/read-limited'
+      DEFAULT_SCOPE = '/authenticate'
 
       option :client_options, {
         :site => 'http://api.orcid.org',
         :authorize_url => 'http://orcid.org/oauth/authorize',
-        :token_url => 'https://api.orcid.org/oauth/token',
-        :scope => '/orcid-profile/read-limited',
+        :token_url => 'https://pub.orcid.org/oauth/token',
+        :scope => '/authenticate',
         :response_type => 'code',
         :mode => :header
       }
 
 
       # Pull out unique ID for the user in the external system
-      uid {  access_token.params["orcid"] }
+      uid { access_token.params["orcid"] }
 
-      info do{} end
+      info { { name: access_token.params["name"] } }
 
       # Customize the parameters passed to the OAuth provider in the authorization phase
       def authorize_params
-        
-        # Trick shamelessly borrowed from the omniauth-facebook gem! 
+
+        # Trick shamelessly borrowed from the omniauth-facebook gem!
         super.tap do |params|
           %w[scope].each { |v| params[v.to_sym] = request.params[v] if request.params[v] }
           params[:scope] ||= DEFAULT_SCOPE # ensure that we're always request *some* default scope
